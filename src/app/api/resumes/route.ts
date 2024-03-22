106a0d7bf5ae4335549e32d5d65597cd2c6d3c41
gitbeginner11 <girtsanowo@gmail.com>
2024-03-23 00:53:23 +0200

    added PATCH to resume

--- src/app/api/resumes/route.ts
+++ src/app/api/resumes/route.ts
@@ -43,7 +43,7 @@ export async function POST(req: Request) {
   return NextResponse.json(createResume, { status: 201 });
  } catch (error) {
   return NextResponse.json(
-   { message: "Something went wrong in api" + error.message },
+   { message: "Something went wrong in api" },
    { status: 500 }
   );
  }
