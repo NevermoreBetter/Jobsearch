7c9209cfae4f5eaf76bfea2a25b7cc8cb0cd6cee
NevermoreBetter <jepaxdd@gmail.com>
2024-05-02 23:09:46 +0300

    fixed bugs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
  images: {
   domains: ["images.clerk.dev"],
  },
+ webpack: (config, options) => {
+  config.externals = config.externals || {};
+  config.externals["@edgestore/server"] = "@edgestore/server";
+  return config;
+ },
 };
 
 export default nextConfig;
